8f57fb87b62b4a2e518f7b5e6c0eaa60b8dab310
TaterTotMan64 <grantburk109@gmail.com>
2024-10-01 18:13:37 -0700

    Constant folding is working properly now

--- optimizer.c
+++ optimizer.c
@@ -44,62 +44,128 @@ void constantFolding(TAC** head) {
     while (current != NULL) {
         getVal(current, &t0, &t1);
 
-        if (current->op != NULL && strcmp(current->op, "+") == 0 || strcmp(current->op, "-") == 0) {
-        
-            
-            printf("%s", current->arg2);
-            printf("\n");
-            printf("$t0: %s\n", t0);
-            printf("$t1: %s\n", t1);
-        if (isConstant(t0) && isConstant(t1)) {
-            // Needs to be optimized
-            int result = 0;
-            if (strcmp(current->op, "+") == 0) {
-                result = atoi(t0) + atoi(t1);
-            } else {
-                result = atoi(t0) - atoi(t1);
+        if (current != NULL && current->op != NULL && strcmp(current->op, "+") == 0 || strcmp(current->op, "-") == 0) {
+            if (isConstant(t0) && isConstant(t1)) {
+                // Needs to be optimized
+                int result = 0;
+                if (strcmp(current->op, "+") == 0) {
+                    result = atoi(t0) + atoi(t1);
+                } else {
+                    result = atoi(t0) - atoi(t1);
+                }
+                char resultStr[20];
+                sprintf(resultStr, "%d", result);
+
+                printf("----Constant Folding----\n");
+                printf("prev_prev_prev = ");
+                printCurrentOptimizedTAC(prev_prev_prev);
+                printf("prev_prev = ");
+                printCurrentOptimizedTAC(prev_prev);
+                printf("prev = ");
+                printCurrentOptimizedTAC(prev);
+                printf("current = ");
+                printCurrentOptimizedTAC(current);
+                printf("----END OF Constant Folding----\n");
+
+                printf("Printing result = %s\n", resultStr);
+
+                // Remove unnecessary instructions
+                if (prev_prev_prev != NULL) {
+                    free(prev);
+                    free(prev_prev);
+                    prev = NULL;
+                    prev_prev = NULL;
+
+                    prev_prev_prev->next = current;
+                } else {
+                    if(prev != NULL) {
+                        free(prev);
+                    }
+                    if(prev_prev != NULL) {
+                        free(prev_prev);
+                    }
+                    *head = current;
+                }
+                current->arg1 = strdup(resultStr);
+                current->arg2 = NULL;
+                current->op = "Num";
+
+                if(strcmp(prev_prev_prev->op, "+") == 0 || strcmp(prev_prev_prev->op, "+") == 0 || strcmp(prev_prev_prev->op, "ID") == 0) {
+                    current->result = strdup("$t0");
+                        t1 = resultStr;
+                        t0 = '\0';
+                } else {
+                    current->result = strdup("$t1");
+                    t1 = resultStr;
+                    t0 = '\0';
+                }
+                printf("Constant folding applied\n");
+                printCurrentOptimizedTAC(current);
             }
-            char resultStr[20];
-            sprintf(resultStr, "%d", result);
-            printf("Printing result = %s\n", resultStr);
-            // Remove unnecessary instructions
-            if (prev_prev_prev != NULL) {
+
+            if (t0 != '\0') {
+                t1 = t0;
+            }
+
+        } else if (prev != NULL && prev->op != NULL && (strcmp(prev->op, "+") == 0 || strcmp(prev->op, "-") == 0)) {
+            printf("----AHHHH----\n");
+            printf("t1 = %s\n", t1);
+            printf("t0 = %s\n", t0);
+            if (isConstant(t0) && isConstant(t1)) {
+                // Needs to be optimized
+                int result = 0;
+                if (strcmp(prev->op, "+") == 0) {
+                    result = atoi(t0) + atoi(t1);
+                } else {
+                    result = atoi(t0) - atoi(t1);
+                }
+                char resultStr[20];
+                sprintf(resultStr, "%d", result);
+
+                printf("----Constant Folding CASE 2----\n");
+                printf("prev_prev_prev = ");
+                printCurrentOptimizedTAC(prev_prev_prev);
+                printf("prev_prev = ");
+                printCurrentOptimizedTAC(prev_prev);
+                printf("prev = ");
+                printCurrentOptimizedTAC(prev);
+                printf("current = ");
+                printCurrentOptimizedTAC(current);
+                printf("t1 = %s\n", t1);
+                printf("t0 = %s\n", t0);
+                printf("----END OF Constant Folding CASE 2----\n");
+
+                printf("Printing result = %s\n", resultStr);
+
                 free(prev);
                 free(prev_prev);
                 prev = NULL;
                 prev_prev = NULL;
-
+                
                 prev_prev_prev->next = current;
-            } else {
-                if(prev != NULL) {
-                    free(prev);
-                }
-                if(prev_prev != NULL) {
-                    free(prev_prev);
-                }
-                *head = current;
-            }
-            current->arg1 = strdup(resultStr);
-            current->arg2 = NULL;
-            current->op = "Num";
-            if(strcmp(prev_prev_prev->op, "+") == 0 || strcmp(prev_prev_prev->op, "+") == 0) {
+
+                current->arg1 = strdup(resultStr);
+                current->arg2 = NULL;
+                current->op = "Num";
                 current->result = strdup("$t0");
-                    t1 = resultStr;
-                    t0 = '\0';
-            } else {
-                current->result = strdup("$t1");
                 t1 = resultStr;
                 t0 = '\0';
+
+                printf("Constant folding applied CASE 2\n");
+                printCurrentOptimizedTAC(current);
             }
-            printf("Constant folding applied\n");
-            printCurrentOptimizedTAC(current);
-        }
-
-        if (t0 != '\0') {
-            t1 = t0;
-        }
 
         }
+
+        printf("----PRINTING PREVS----\n");
+        printf("prev_prev_prev = ");
+        printCurrentOptimizedTAC(prev_prev_prev);
+        printf("prev_prev = ");
+        printCurrentOptimizedTAC(prev_prev);
+        printf("prev = ");
+        printCurrentOptimizedTAC(prev);
+        printf("----END PRINTING PREVS----\n");
+
         if(prev_prev != NULL) {
             prev_prev_prev = prev_prev;
         }
@@ -107,9 +173,7 @@ void constantFolding(TAC** head) {
         prev = current;
         current = current->next;
     }
-    free(prev);
-    free(prev_prev);
-    free(prev_prev_prev);
+
     free(t1);
 }
 
@@ -159,7 +223,9 @@ void constantPropagation(TAC** head) {
     }
 }
 void printCurrentOptimizedTAC(TAC* tac) {
-    if (strcmp(tac->op, "VarDecl") == 0) {
+    if (tac == NULL) {
+        printf("NULL\n");
+    } else if (strcmp(tac->op, "VarDecl") == 0) {
         printf("%s %s ==> %s\n", tac->arg1, tac->arg2, tac->result);
     } else if (strcmp(tac->op, "=") == 0) {
         printf("%s (%s) = %s\n", tac->result, tac->arg1, tac->arg2);
